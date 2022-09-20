ea867af80669c70e2babe238f9e3c90a2a20f286
Jicell <jicellcollado@Jicells-MacBook-Air.local>
2022-09-20 16:58:45 -0400

    Creating Model Controller

---
+++ CohortList/CohortList/Model/Student.swift
@@ -0,0 +1,19 @@
+//
+//  Student.swift
+//  CohortList
+//
+//  Created by Jicell on 9/20/22.
+//
+
+import UIKit
+
+class Student{
+    
+    let name: String
+    let cohortID: Int
+    
+    init(name: String, cohortID: Int) {
+        self.name = name
+        self.cohortID = cohortID
+    }
+}
